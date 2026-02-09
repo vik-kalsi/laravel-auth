837143f63d6bfe3a4126950b8c8913faef7a9bed
vik-kalsi <kalsivik@googlemail.com>
2026-02-09 20:58:06 +0000

    Adding Registration function

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,10 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
 
 class RegisterController extends Controller
 {
     public function OpenRegistrationPage(){
         return view('pages.registration');
+    }
+
+
+    public function RegisterAccount(Request $request){
+
+        $request->validate([
+            'username' => 'required|string|max:20',
+            'password' => 'required|confirmed|min:8',
+        ]);
+
+
+        $usernameExists = DB::table('users_tb')->where('username', $request->username)->first();
+
+
+        if(!$usernameExists){ 
+            $userID = DB::table('users_tb')->insert([
+                'username' => $request->username,
+                'password' => Hash::make($request->password),
+            ]);
+            
+            return redirect()->action([LoginController::class, 'OpenLoginPage'])
+            ->with('registerSuccess', 'Account has been registered succesfully, please login');
+        
+        } else {
+            return redirect()->action([RegisterController::class, 'RegisterAccount'])
+            ->withErrors(['usernameAlreadyExists' => "This Username already exists, please choose a different username"])
+            ->withInput(); 
+        }
+
     }
 }
