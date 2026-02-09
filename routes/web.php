b9add65b360084ad8f019f4c17bf0d6ed02be609
vik-kalsi <kalsivik@googlemail.com>
2026-02-09 20:36:36 +0000

    Creating to open pages and creating user table

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/login', [LoginController::class, 'OpenLoginPage']);
+Route::post('/login', [LoginController::class, 'LoginCheck']);
+
+
+Route::get('/register', [RegisterController::class, "OpenRegistrationPage"]);
+Route::post('/register', [RegisterController::class, 'RegisterAccount']);
